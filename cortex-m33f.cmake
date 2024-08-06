7117849c2ad39d581cae1d8c6d072b39f5038734
Hossein Sagha <37206671+HosseinSagha@users.noreply.github.com>
2024-08-06 11:03:28 +1000

    fix m33 fpu version

--- cortex-m33f.cmake
+++ cortex-m33f.cmake
@@ -5,7 +5,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
 set(MCU_ARCH cortex-m33)
-set(CPU_FLAGS "-mthumb -mcpu=${MCU_ARCH} -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+set(CPU_FLAGS "-mthumb -mcpu=${MCU_ARCH} -mfloat-abi=hard -mfpu=fpv5-sp-d16")
 set(SPEC_FLAGS_INIT "--specs=nosys.specs")
 set(SPEC_FLAGS "--specs=nano.specs")
 
